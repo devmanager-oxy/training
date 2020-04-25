c94d0173c1b857cd32d2f78178973573f1c0fe35
aryawiratama <gede.arya.wiratama@gmail.com>
2020-04-25 12:56:13 +0800

    add liquibase config

--- src/main/java/com/oxy/customer/Customer.java
+++ src/main/java/com/oxy/customer/Customer.java
@@ -35,6 +35,9 @@ public class Customer extends GlobalEntity {
     @Column(name="email", unique = true)
     private String email;
 
+    @Column(name = "phone")
+    private String phone;
+
     public String getName() {
         return name;
     }
@@ -49,5 +52,21 @@ public class Customer extends GlobalEntity {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
